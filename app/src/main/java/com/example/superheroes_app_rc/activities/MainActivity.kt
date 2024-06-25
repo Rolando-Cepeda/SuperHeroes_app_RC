417fc5873c1e9da7e887d332783eb250938a0724
rolando_cf <rolandodj83@gmail.com>
2024-06-25 10:28:14 +0200

    Project_3.- Ya hemos pintado la lista de superheroes aunque sea mostrando solo el nombre Y hemos activado el ViewBinding

--- app/src/main/java/com/example/superheroes_app_rc/activities/MainActivity.kt
+++ app/src/main/java/com/example/superheroes_app_rc/activities/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.superheroes_app_rc.activities
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.superheroes_app_rc.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.superheroes_app_rc.adapters.SuperheroAdapter
 import com.example.superheroes_app_rc.data.SuperheroApiService
+import com.example.superheroes_app_rc.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,9 +13,22 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var adapter: SuperheroAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = SuperheroAdapter()
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = GridLayoutManager(this,2)
+
 
         searchByName("super")
     }
@@ -25,7 +39,11 @@ class MainActivity : AppCompatActivity() {
             try {
                 val apiService = getRetrofit().create(SuperheroApiService::class.java)
                 val result = apiService.findSuperheroesByName(query)
-                Log.i("HTTP", "${result.results}")
+
+                runOnUiThread {
+                    adapter.updateData(result.results)
+                }
+                // Log.i("HTTP", "${result.results}")
             } catch (e: Exception) {
                 e.printStackTrace()
             }
